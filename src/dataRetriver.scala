5624e04e306163cf42c8d7ccb7832f3db42449e0
JCKolodziej <49226039+JCKolodziej@users.noreply.github.com>
2020-03-17 12:48:16 +0100

    changed to sbt project

--- src/dataRetriver.scala
+++
@@ -1,7 +0,0 @@
-object dataRetriver extends App{
-  val url = "https://ghoapi.azureedge.net/api/DIMENSION/COUNTRY/DimensionValues"
-  val result = scala.io.Source.fromURL(url)
-  var res = result.filter(x => x == "Ago")
-  println(result.value)
-  println(res)
-}
